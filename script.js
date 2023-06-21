a707cc19553e8070696df81373aafbd75222e4a9
Rohit kishne <92744319+rohitkishne@users.noreply.github.com>
2023-06-21 17:01:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	let letter = {
+		"2" : "abc",
+		"3" : "def",
+		"4" : "ghi",
+		"5" : "jkl",
+		"6" : "mno",
+		"7" : "pqrs",
+		"8" : "tuv",
+		"9" : "wxyz"
+	}
+
+	let ans = [];
+
+      function solve(currentIdx, currentString) {
+        if (input_digit.length<=currentIdx) {
+          ans.push(currentString);
+          return;
+        }
+        let currentChar = input_digit[currentIdx];
+        for (let i = 0; i < letter[currentChar].length; i++) {
+          solve(currentIdx + 1, currentString + letter[currentChar][i]);
+        }
+      }
+      solve(0, "");
+	return ans;
 }
 
 module.exports = letterCombinations;
